58204bd6b8b30f6a0a77f1374b5bfee935c6b8d5
vicgui <guijarrovictormon@gmail.com>
2022-07-25 20:43:24 +0200

    new endpoints added for accommodation

--- backend/server/src/resources/components/accommodation/accommodation.router.v1.js
+++ backend/server/src/resources/components/accommodation/accommodation.router.v1.js
@@ -11,10 +11,21 @@ const Accommodation = require("./accommodation.service")
     await Accommodation.createAccommodation(accommData);
     res.status(200).json({ status: `Accomm created` });
   };
+  const geAllAccommodations = async (req, res) => {
+    const docs = await Accommodation.findAll();
+    res.status(200).json({ results: [docs] });
+  };
 
+  const getAccommodationByTravel = async(req, res) =>{
+    const {idTravel} = req.body;
+    const doc = await Accommodation.findByTravelId(idTravel);
+    res.status(200).json({ results: [doc] });
+  }
   
   const router = express.Router();
   
   router.post("/create", catchErrors(create));
-  
+  router.get("/getAll", catchErrors(geAllAccommodations));
+  router.get("/getByTravelId", catchErrors(getAccommodationByTravel));
+
   module.exports = router;
