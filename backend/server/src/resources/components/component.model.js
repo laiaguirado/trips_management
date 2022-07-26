9bbd2908535d8d339daefb60f7c1b2bcf230d9b8
Natalia <naguado@gmail.com>
2022-07-26 12:10:04 +0200

    Remove package mongoose-unique-validator

--- backend/server/src/resources/components/component.model.js
+++ backend/server/src/resources/components/component.model.js
@@ -1,29 +1,26 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-const extendSchema = require('mongoose-extend-schema');
-
+const extendSchema = require("mongoose-extend-schema");
 
 const componentSchema = mongoose.Schema(
-    {
-      resourceType: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        immutable: true,
-      },
-      idTravel: {
-        type: String,
-        required: true,
-      },
-      idUser: {
-        type: String,
-        required: true,
-      },
-   
+  {
+    resourceType: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      immutable: true,
     },
-    { timestamps: true }
-  );
+    idTravel: {
+      type: String,
+      required: true,
+    },
+    idUser: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-  const Component = mongoose.model("component",componentSchema);
-  module.exports = {Component,componentSchema};
+const Component = mongoose.model("component", componentSchema);
+module.exports = { Component, componentSchema };
