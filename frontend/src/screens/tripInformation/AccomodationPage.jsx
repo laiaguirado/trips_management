73ce629aebc3f0bb187ae2ebc3a9de1d345b0091
Alba Talaya <talayaalba@gmail.com>
2022-07-27 01:39:18 +0200

    accomodation page

--- frontend/src/screens/tripInformation/AccomodationPage.jsx
+++ frontend/src/screens/tripInformation/AccomodationPage.jsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Bar from "../../components/Bar";
 import "./AccomodationPage.css";
 
 function AccomodationPage() {
+  const editPage = () => {
+    console.log("editing");
+    //todo change displayed values for inputs
+    //todosave input
+  };
+
   return (
     <div className="accomodation-page">
       <Bar />
-      <h1>ACCOMODATION</h1>
-      <h2>Dates: </h2>
-      <h2>Address: </h2>
-      <h2>Contact: </h2>
-      <h2>Notes: </h2>
+      <div className="flex-container">
+        <div>
+          <h1>ACCOMODATION</h1>
+
+          <h2>
+            Name: <span>Ruthensteiner</span>
+          </h2>
+          <h2>
+            Dates: <span>10/10/2022 - 15/10/2022</span>
+          </h2>
+          <h2>
+            Address:
+            <span> Robert Hamerling Gasse 24, 1150 Vienna, Austria</span>
+          </h2>
+          <h2>
+            Contact:
+            <span>
+              <a href="mailto:contact@gmail.com"> contact@gmail.com</a>
+            </span>
+          </h2>
+        </div>
+        <div>
+          <img src="../../src/assets/accomodation.webp"></img>
+        </div>
+      </div>
+      <h2>
+        Notes:{" "}
+        <span>
+          You have to arrive after 14:00 if you want to get the keys to your
+          room.
+        </span>
+      </h2>
       <h2>Files: </h2>
+      <div className="upload">Upload file</div>
       <button onClick={() => window.history.go(-1)}>Go back</button>
+      <button onClick={() => editPage()}>Edit</button>
     </div>
   );
 }
