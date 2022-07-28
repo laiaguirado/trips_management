693e1fc2e3408e882e3b13c94d4d30dd93145c66
Natalia <naguado@gmail.com>
2022-07-29 00:47:10 +0200

    Change response code status on create operation

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,41 +4,41 @@ const BASE_URL = `http://${HOST}:${PORT}`;
 const version = "v1";
 
 export const login = async (userData) => {
-    try {
-        const response = await fetch(`${BASE_URL}/user/${version}/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userData),
-        });
-        const result = await response.json();
-        if (response.status === 200) {
-            return { success: true, token: result };
-        } else {
-            return { success: false, error: result.error };
-        }
-    } catch (e) {
-        return { success: false, error: e.message };
+  try {
+    const response = await fetch(`${BASE_URL}/user/${version}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const result = await response.json();
+    if (response.status === 200) {
+      return { success: true, token: result };
+    } else {
+      return { success: false, error: result.error };
     }
+  } catch (e) {
+    return { success: false, error: e.message };
+  }
 };
 
 export const register = async (userData) => {
-    try {
-        const response = await fetch(`${BASE_URL}/user/${version}/register`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userData),
-        });
-        const result = await response.json();
-        if (response.status === 200) {
-            return { success: true };
-        } else {
-            return { success: false, error: result.error };
-        }
-    } catch (e) {
-        return { success: false, error: e.message };
+  try {
+    const response = await fetch(`${BASE_URL}/user/${version}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const result = await response.json();
+    if (response.status === 201) {
+      return { success: true };
+    } else {
+      return { success: false, error: result.error };
     }
+  } catch (e) {
+    return { success: false, error: e.message };
+  }
 };
