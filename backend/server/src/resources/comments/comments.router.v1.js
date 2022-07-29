33ba93ebdf081e80ab054400107fc3b447cc0844
vicgui <guijarrovictormon@gmail.com>
2022-07-29 17:31:19 +0200

    corrections backend

--- backend/server/src/resources/comments/comments.router.v1.js
+++ backend/server/src/resources/comments/comments.router.v1.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { route } = require("express/lib/application");
 const express = require("express");
 const { catchErrors, TripManagementApiError } = require("../../errors");
+const { needsAuthToken } = require("../users/auth/auth.middleware");
 
 const Comment = require("./comments.service");
 const Accomodation = require("../components/accommodation/accommodation.service")
@@ -9,8 +10,8 @@ const User = require("../users/user.service")
 
 const create = async(req,res)=>{
     const compId = req.params.id
+    const { email, _id, username } = req.userInfo;
     const { comment_text } = req.body;
-    const { user_id } = req.body;
     console.log(compId);
     console.log(comment_text );
 
@@ -18,7 +19,7 @@ const create = async(req,res)=>{
 
     const accommodation = await Accomodation.findOneById(compId);
 
-    const user = await User.findById(user_id);
+    const user = await User.findById(_id);
 
     accommodation.comments.push(comment);
     await accommodation.save()
@@ -26,7 +27,7 @@ const create = async(req,res)=>{
     user.comments.push(comment);
     await user.save()
 
-    res.status(200).json({ status: `User ${user_id} added Comment ${comment._id} to resource: ${compId}` });
+    res.status(200).json({ status: `User ${username} added Comment ${comment._id} to resource: ${compId}` });
    
 }
 
@@ -38,7 +39,7 @@ const getAll = async(req,res) =>{
 
 const router = express.Router();
 
-router.post("/create/:id", catchErrors(create));
+router.post("/:id", needsAuthToken, catchErrors(create));
 router.get("/getAll", catchErrors(getAll));
 
 module.exports = router;
--- backend/server/src/resources/score/score.router.v1.js
+++ backend/server/src/resources/score/score.router.v1.js
@@ -2,23 +2,26 @@ const { Router } = require("express");
 const { route } = require("express/lib/application");
 const express = require("express");
 const { catchErrors, TripManagementApiError } = require("../../errors");
+const { needsAuthToken } = require("../users/auth/auth.middleware");
 
 const Score = require("./score.service");
 const Accomodation = require("../components/accommodation/accommodation.service");
 const User = require("../users/user.service")
 
 const create = async(req,res)=>{
-    const compId = req.params.id
+    const compId = req.params._id;
+    const { email, _id, username } = req.userInfo;
     const { value } = req.body;
-    const {user_id} = req.body;
     console.log(compId);
     console.log(value );
 
-    const score = await Score.createOne(value,compId,user_id);
+    const score = await Score.createOne(value,compId,_id);
+    console.log(score)
 
     const accommodation = await Accomodation.findOneById(compId);
+    console.log(accommodation)
 
-    const user = await User.findById(user_id);   
+    const user = await User.findById(_id);   
 
     accommodation.scores.push(score);
     await accommodation.save();
@@ -27,7 +30,7 @@ const create = async(req,res)=>{
     await user.save();
     console.log(user);
 
-    res.status(200).json({ status: `User: ${user_id} added Score ${score._id} to resource: ${compId}` });
+    res.status(200).json({ status: `User: ${username} added Score ${score._id} to resource: ${compId}` });
    
 }
 
@@ -39,7 +42,7 @@ const getAll = async(req,res) =>{
 
 const router = express.Router();
 
-router.post("/create/:id", catchErrors(create));
+router.post("/:_id",needsAuthToken, catchErrors(create));
 router.get("/getAll", catchErrors(getAll));
 
 module.exports = router;
