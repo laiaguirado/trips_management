632ffe86f1f99b900991ca005a37d482ef17db11
Natalia <naguado@gmail.com>
2022-08-17 22:25:06 +0200

    Refactory endpoints Accommodation

--- backend/server/src/resources/components/accommodation/accommodation.service.js
+++ backend/server/src/resources/components/accommodation/accommodation.service.js
@@ -1,4 +1,5 @@
 const Accommodation = require("./accommodation.model");
+const { errMalformed } = require("../../../errors");
 
 const createAccommodation = async ({ web, description, location, startDate, endDate, phone, email, resourceType }, idTravel, idUser) => {
   return await Accommodation.create({ web, description, location, startDate, endDate, phone, email, resourceType, idTravel, idUser });
@@ -10,38 +11,25 @@ const findAll = async () => {
 
 const findByTravelId = async (_idTravel) => {
   return await Accommodation.find({ idTravel: _idTravel }).exec();
-}
+};
 
 const findOneById = async (id) => {
-  return await Accommodation.findOne({ _id: id })
-}
+  return await Accommodation.findOne({ _id: id });
+};
 
 const deleteAccom = async (_id) => {
   const deleted = await Accommodation.findByIdAndDelete({ _id }).lean().exec();
   if (deleted === null) {
-    errMalformed(`Accommodation with '${_id}' not found`);
+    errMalformed(`Accommodation not found`);
   }
   return deleted;
 };
 
-const updateAccomodation = async ({
-  _id,
-  web,
-  description,
-  location,
-  startDate,
-  endDate,
-  phone,
- email
-}) => {
-  const accomUpdated = await Accommodation.findOneAndUpdate(
-    { _id },
-    {web, description, location,startDate,endDate, phone, email},{new:true}
-  ).lean()
-    .exec();
+const updateAccomodation = async (_id, accommodationData) => {
+  const accomUpdated = await Accommodation.findOneAndUpdate({ _id }, accommodationData, { new: true }).lean().exec();
 
   if (accomUpdated === null) {
-    errMalformed(`Accomodation with id '${_id}' not found`);
+    errMalformed(`Accomodation not found`);
   }
   return accomUpdated;
 };
