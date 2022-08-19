544aa5b9106a2c45bf4c06473f2f4a798f8bd14d
Alba Talaya <talayaalba@gmail.com>
2022-08-20 00:12:00 +0200

    Style Add Restoration Page

--- frontend/src/components/tripInformation/Restoration/AddRestorationCard.jsx
+++ frontend/src/components/tripInformation/Restoration/AddRestorationCard.jsx
@@ -13,6 +13,7 @@ import {
   faAngleLeft,
   faNoteSticky,
   faEnvelope,
+  faDollar,
 } from "@fortawesome/free-solid-svg-icons";
 
 function AddRestorationCard({ onAdd, adding, tripId }) {
@@ -48,16 +49,18 @@ function AddRestorationCard({ onAdd, adding, tripId }) {
   };
 
   return (
-    <div className="add-restoration-card">
-      <div className="return-icon" onClick={adding}>
-        <FontAwesomeIcon icon={faAngleLeft} size="3x" />{" "}
-      </div>
-      <form className="add-restoration" onSubmit={(e) => add(e)}>
-        <h1>New Restoration</h1>
-        {/*<label>
+    <div className="add-card add-restoration-card">
+      <div className="form-container">
+        <div className="return-icon" onClick={adding}>
+          <FontAwesomeIcon icon={faAngleLeft} size="3x" />{" "}
+        </div>
+        <form className="add-form" onSubmit={(e) => add(e)}>
+          <h1 className="title">New Restoration</h1>
+          {/*<label>
           <div className="form-data">
-            <FontAwesomeIcon icon={faUtensils} />
+            <FontAwesomeIcon icon={faUtensils} className="icon" />
             <input
+                className="input"
               type="text"
               placeholder="Name"
               value={name}
@@ -65,118 +68,139 @@ function AddRestorationCard({ onAdd, adding, tripId }) {
             />
           </div>
   </label>*/}
-        <label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faBurger} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Kind of Food"
+                value={kindOfFood}
+                onChange={(event) => setKindOfFood(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faNoteSticky} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faLocationDot} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Location"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faSackDollar} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Min price"
+                value={minPrice}
+                onChange={(event) => setMinPrice(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faDollar} className="icon" />
+              <select
+                className="input date"
+                required
+                value={currency}
+                onChange={(event) => setCurrency(event.target.value)}
+              >
+                <option value="Select...">Select a currency</option>
+                <option value="€">Euro €</option>
+                <option value="$">Dollar $</option>
+                <option value="£">Libra £</option>
+              </select>
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faSackDollar} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Max price"
+                value={maxPrice}
+                onChange={(event) => setMaxPrice(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faDollar} className="icon" />
+              <select
+                className="input date"
+                required
+                value={currency}
+                onChange={(event) => setCurrency(event.target.value)}
+              >
+                <option value="Select...">Select a currency</option>
+                <option value="€">Euro €</option>
+                <option value="$">Dollar $</option>
+                <option value="£">Libra £</option>
+              </select>
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faGlobe} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Web"
+                value={web}
+                onChange={(event) => setWeb(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faPhone} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Phone number"
+                value={phone}
+                onChange={(event) => setPhone(event.target.value)}
+              />
+            </div>
+          </label>
+          <label>
+            <div className="form-data">
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+          </label>
+          {/*<label>
           <div className="form-data">
-            <FontAwesomeIcon icon={faBurger} />
+            <FontAwesomeIcon icon={faNoteSticky} className="icon" />
             <input
-              type="text"
-              placeholder="Kind of Food"
-              value={kindOfFood}
-              onChange={(event) => setKindOfFood(event.target.value)}
-            />
-          </div>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faNoteSticky} />
-            <input
-              type="text"
-              placeholder="Description"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-            />
-          </div>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faLocationDot} />
-            <input
-              type="text"
-              placeholder="Location"
-              value={location}
-              onChange={(event) => setLocation(event.target.value)}
-            />
-          </div>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faSackDollar} />
-            <input
-              type="text"
-              placeholder="Min price"
-              value={minPrice}
-              onChange={(event) => setMinPrice(event.target.value)}
-            />
-          </div>
-          <select
-            required
-            value={currency}
-            onChange={(event) => setCurrency(event.target.value)}
-          >
-            <option value="Select...">Select a currency</option>
-            <option value="€">Euro €</option>
-            <option value="$">Dollar $</option>
-            <option value="£">Libra £</option>
-          </select>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faSackDollar} />
-            <input
-              type="text"
-              placeholder="Max price"
-              value={maxPrice}
-              onChange={(event) => setMaxPrice(event.target.value)}
-            />
-          </div>
-          <select
-            required
-            value={currency}
-            onChange={(event) => setCurrency(event.target.value)}
-          >
-            <option value="Select...">Select a currency</option>
-            <option value="€">Euro €</option>
-            <option value="$">Dollar $</option>
-            <option value="£">Libra £</option>
-          </select>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faGlobe} />
-            <input
-              type="text"
-              placeholder="Web"
-              value={web}
-              onChange={(event) => setWeb(event.target.value)}
-            />
-          </div>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faPhone} />
-            <input
-              type="text"
-              placeholder="Phone number"
-              value={phone}
-              onChange={(event) => setPhone(event.target.value)}
-            />
-          </div>
-        </label>
-        <label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-        </label>
-        {/*<label>
-          <div className="form-data">
-            <FontAwesomeIcon icon={faNoteSticky} />
-            <input
+                className="input"
               type="text"
               placeholder="Notes"
               value={notation}
@@ -184,8 +208,13 @@ function AddRestorationCard({ onAdd, adding, tripId }) {
             />
           </div>
   </label>*/}
-        <input type="submit" value="Create New Restoration" />
-      </form>
+          <input
+            className="submit-button form-data"
+            type="submit"
+            value="Create New Restoration"
+          />
+        </form>
+      </div>
     </div>
   );
 }
