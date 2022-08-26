faa466b9135f31fef2e60d9507ca05dd687c31a6
Natalia <naguado@gmail.com>
2022-08-26 09:04:20 +0200

    Define enum for accommodation type

--- backend/server/src/resources/components/accommodation/accommodation.model.js
+++ backend/server/src/resources/components/accommodation/accommodation.model.js
@@ -14,74 +14,90 @@ function getPriceAccommodationWithCurrency() {
   return getPriceWithCurrency(this.price, this.currency);
 }
 
-const accommodationSchema = extendSchema(componentSchema, {
-  name: {
-    type: String,
-    maxlength: [50, "{PATH} is too long"],
-    required: [true, "{PATH} is required"],
-    set: capitalize,
+const accommodationSchema = extendSchema(
+  componentSchema,
+  {
+    name: {
+      type: String,
+      maxlength: [50, "{PATH} is too long"],
+      required: [true, "{PATH} is required"],
+      set: capitalize,
+    },
+    notation: {
+      type: String,
+      maxlength: [500, "'{PATH}' is too long"],
+    },
+    location: {
+      type: String,
+      required: [true, "{PATH} is required"],
+      maxlength: [200, "{PATH} is too long"],
+    },
+    startDate: {
+      type: Date,
+    },
+    endDate: {
+      type: Date,
+    },
+    web: {
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      trim: true,
+    },
+    email: {
+      type: String,
+      uniqueCaseInsensitive: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, " '{PATH}' is invalid"],
+      immutable: true,
+    },
+    price: {
+      type: Number,
+    },
+    petFriendly: {
+      type: Boolean,
+    },
+    checkInHour: {
+      type: String,
+    },
+    checkOutHour: {
+      type: String,
+    },
+    internet: {
+      type: Boolean,
+    },
+    swimmingPool: {
+      type: Boolean,
+    },
+    type: {
+      type: String,
+      enum: {
+        values: [
+          "Hotel",
+          "Apartment",
+          "Camping",
+          "Bungalow",
+          "Hostal",
+          "Chalets",
+          "Cottages",
+          "Other",
+        ],
+        message: "{VALUE} for {TYPE} is not suported",
+      },
+    },
+    breakfast: {
+      type: Boolean,
+    },
   },
-  notation: {
-    type: String,
-    maxlength: [500, "'{PATH}' is too long"],
-  },
-  location: {
-    type: String,
-    required: [true, "{PATH} is required"],
-    maxlength: [200, "{PATH} is too long"],
-  },
-  startDate: {
-    type: Date
-  },
-  endDate: {
-    type: Date
-  },
-  web: {
-    type: String,
-    trim: true,
-  },
-  phone: {
-    type: Number,
-    trim: true,
-  },
-  email: {
-    type: String,
-    uniqueCaseInsensitive: true,
-    trim: true,
-    lowercase: true,
-    match: [/\S+@\S+\.\S+/, " '{PATH}' is invalid"],
-    immutable: true,
-  },
-  price: {
-    type: Number
-  },
-  petFriendly: {
-    type: Boolean
-  },
-  checkInHour: {
-    type: String
-  },
-  checkOutHour: {
-    type: String
-  },
-  internet: {
-    type: Boolean
-  },
-  swimmingPool: {
-    type: Boolean
-  },
-  type: {
-    type: String
-  },
-  breakfast: {
-    type: Boolean
-  }
-},
   {
     toJSON: { getters: true, setters: true, virtuals: true },
     toObject: { getters: true, setters: true, virtuals: true },
     runSettersOnQuery: true,
-  });
+  }
+);
 
 accommodationSchema.index({ phone: 1 });
 accommodationSchema.plugin(mongooseLeanGetters);
