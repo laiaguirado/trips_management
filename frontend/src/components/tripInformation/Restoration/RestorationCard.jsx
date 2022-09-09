57430ef4f3d14c38a7e62044c9a11433268ccd42
Alba Talaya <talayaalba@gmail.com>
2022-09-09 15:07:56 +0200

    Added illustrations to details pages

--- frontend/src/components/tripInformation/Restoration/RestorationCard.jsx
+++ frontend/src/components/tripInformation/Restoration/RestorationCard.jsx
@@ -58,121 +58,129 @@ function RestorationCard({ restoration }) {
         <div className="dot"></div>
       </div>
 
-      <div className="restoration-info">
-        <div className="restoration-location restoration-detail">
-          <h3>Location: </h3>
-          <div>{restoration.location}</div>
+      <div className="details-flex">
+        <div className="restoration-info">
+          <div className="restoration-location restoration-detail">
+            <h3>Location: </h3>
+            <div>{restoration.location}</div>
+          </div>
+          {restoration.kindOfFood && (
+            <div className="restoration-kindOfFood restoration-detail">
+              <h3>Kind of Food: </h3>
+              <div>{restoration.kindOfFood}</div>
+            </div>
+          )}
+          {(restoration.minPriceWithCurrency ||
+            restoration.maxPriceWithCurrency) && (
+            <div className="restoration-price restoration-detail">
+              <h3>Price (min - max):</h3>
+              <div>{getPrices()}</div>
+            </div>
+          )}
+          {(restoration.openingHour || restoration.closingHour) && (
+            <div className="restoration-hours restoration-detail">
+              <h3>Opening hour - Closing Hour:</h3>
+              <div>{getHours()}</div>
+            </div>
+          )}
+          {restoration.closed && (
+            <div className="restoration-closed restoration-detail">
+              <h3>Closed: </h3>
+              <div>{restoration.closed}</div>
+            </div>
+          )}
+          {restoration.speciality && (
+            <div className="restoration-speciality restoration-detail">
+              <h3>Speciality of the restaurant: </h3>
+              <div>{restoration.speciality}</div>
+            </div>
+          )}
+          {restoration.takeAway !== null && (
+            <div className="restoration-takeAway restoration-detail">
+              <h3>Take Away: </h3>
+              <div>
+                {restoration.takeAway !== null &&
+                restoration.takeAway !== undefined
+                  ? restoration.takeAway === true
+                    ? "Yes"
+                    : "No"
+                  : ""}
+              </div>
+            </div>
+          )}
+          {restoration.reserved !== null && (
+            <div className="restoration-reserved restoration-detail">
+              <h3>Reservation required: </h3>
+              <div>
+                {restoration.reserved !== null &&
+                restoration.reserved !== undefined
+                  ? restoration.reserved === true
+                    ? "Yes"
+                    : "No"
+                  : ""}
+              </div>
+            </div>
+          )}
+          {restoration.notation && (
+            <div className="restoration-notation restoration-detail">
+              <h3>Notes: </h3>
+              <div className="notation-text">{restoration.notation}</div>
+            </div>
+          )}
         </div>
-        {restoration.kindOfFood && (
-          <div className="restoration-kindOfFood restoration-detail">
-            <h3>Kind of Food: </h3>
-            <div>{restoration.kindOfFood}</div>
-          </div>
-        )}
-        {(restoration.minPriceWithCurrency ||
-          restoration.maxPriceWithCurrency) && (
-          <div className="restoration-price restoration-detail">
-            <h3>Price (min - max):</h3>
-            <div>{getPrices()}</div>
-          </div>
-        )}
-        {(restoration.openingHour || restoration.closingHour) && (
-          <div className="restoration-hours restoration-detail">
-            <h3>Opening hour - Closing Hour:</h3>
-            <div>{getHours()}</div>
-          </div>
-        )}
-        {restoration.closed && (
-          <div className="restoration-closed restoration-detail">
-            <h3>Closed: </h3>
-            <div>{restoration.closed}</div>
-          </div>
-        )}
-        {restoration.speciality && (
-          <div className="restoration-speciality restoration-detail">
-            <h3>Speciality of the restaurant: </h3>
-            <div>{restoration.speciality}</div>
-          </div>
-        )}
-        {restoration.takeAway !== null && (
-          <div className="restoration-takeAway restoration-detail">
-            <h3>Take Away: </h3>
-            <div>
-              {restoration.takeAway !== null &&
-              restoration.takeAway !== undefined
-                ? restoration.takeAway === true
-                  ? "Yes"
-                  : "No"
-                : ""}
-            </div>
-          </div>
-        )}
-        {restoration.reserved !== null && (
-          <div className="restoration-reserved restoration-detail">
-            <h3>Reservation required: </h3>
-            <div>
-              {restoration.reserved !== null &&
-              restoration.reserved !== undefined
-                ? restoration.reserved === true
-                  ? "Yes"
-                  : "No"
-                : ""}
-            </div>
-          </div>
-        )}
-        {restoration.notation && (
-          <div className="restoration-notation restoration-detail">
-            <h3>Notes: </h3>
-            <div className="notation-text">{restoration.notation}</div>
-          </div>
-        )}
-        {(restoration.web || restoration.phone || restoration.email) && (
-          <div className="restoration-contact">
-            {restoration.web && (
-              <div>
-                {restoration !== "" ? (
-                  !restoration.web.startsWith("https://") &&
-                  !restoration.web.startsWith("http://") &&
-                  restoration.web !== null &&
-                  restoration.web !== "" ? (
-                    <a href={"https://" + restoration.web} target="_blank">
-                      <FontAwesomeIcon icon={faGlobe} className="icon" />
-                      <div>Web:</div>
-                      <div className="link">{restoration.web}</div>
-                    </a>
-                  ) : (
-                    <a href={restoration.web} target="_blank">
-                      <FontAwesomeIcon icon={faGlobe} className="icon" />
-                      <div>Web:</div>
-                      <div className="link">{restoration.web}</div>
-                    </a>
-                  )
-                ) : (
-                  <p></p>
-                )}
-              </div>
-            )}
-            {restoration.phone && (
-              <div>
-                <a href={"tel:" + restoration.phone}>
-                  <FontAwesomeIcon icon={faPhone} className="icon" />
-                  <div>Phone:</div>
-                  <div className="link">{restoration.phone}</div>
-                </a>
-              </div>
-            )}
-            {restoration.email && (
-              <div>
-                <a href={"mailto:" + restoration.email}>
-                  <FontAwesomeIcon icon={faEnvelope} className="icon" />
-                  <div>Email:</div>
-                  <div className="link">{restoration.email}</div>
-                </a>
-              </div>
-            )}
-          </div>
-        )}
+
+        <img
+          className="details-illustration"
+          src={"../../../src/assets/illustrations/restoration.svg"}
+        ></img>
       </div>
+
+      {(restoration.web || restoration.phone || restoration.email) && (
+        <div className="restoration-contact">
+          {restoration.web && (
+            <div>
+              {restoration !== "" ? (
+                !restoration.web.startsWith("https://") &&
+                !restoration.web.startsWith("http://") &&
+                restoration.web !== null &&
+                restoration.web !== "" ? (
+                  <a href={"https://" + restoration.web} target="_blank">
+                    <FontAwesomeIcon icon={faGlobe} className="icon" />
+                    <div>Web:</div>
+                    <div className="link">{restoration.web}</div>
+                  </a>
+                ) : (
+                  <a href={restoration.web} target="_blank">
+                    <FontAwesomeIcon icon={faGlobe} className="icon" />
+                    <div>Web:</div>
+                    <div className="link">{restoration.web}</div>
+                  </a>
+                )
+              ) : (
+                <p></p>
+              )}
+            </div>
+          )}
+          {restoration.phone && (
+            <div>
+              <a href={"tel:" + restoration.phone}>
+                <FontAwesomeIcon icon={faPhone} className="icon" />
+                <div>Phone:</div>
+                <div className="link">{restoration.phone}</div>
+              </a>
+            </div>
+          )}
+          {restoration.email && (
+            <div>
+              <a href={"mailto:" + restoration.email}>
+                <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                <div>Email:</div>
+                <div className="link">{restoration.email}</div>
+              </a>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/tripInformation/Transportation/TransportationCard.jsx
+++ frontend/src/components/tripInformation/Transportation/TransportationCard.jsx
@@ -97,64 +97,70 @@ function TransportationCard({ transportation }) {
         <div className="dot"></div>
       </div>
 
-      <div className="transportation-info">
-        <div className="transportation-type transportation-detail">
-          <h3>Type: </h3>
-          <div>{transportation.type}</div>
-        </div>
-        {transportation.typeDetails && (
-          <div className="transportation-typeDetails transportation-detail">
-            <h3>Type Details: </h3>
-            <div>{transportation.typeDetails}</div>
+      <div className="details-flex">
+        <div className="transportation-info">
+          <div className="transportation-type transportation-detail">
+            <h3>Type: </h3>
+            <div>{transportation.type}</div>
           </div>
-        )}
-        {transportation.priceWithCurrency && (
-          <div className="transportation-price transportation-detail">
-            <h3>Price: </h3>
-            <div>
-              {transportation.priceWithCurrency !== ""
-                ? transportation.priceWithCurrency
-                : ""}
+          {transportation.typeDetails && (
+            <div className="transportation-typeDetails transportation-detail">
+              <h3>Type Details: </h3>
+              <div>{transportation.typeDetails}</div>
             </div>
-          </div>
-        )}
-        {transportation.notation && (
-          <div className="transportation-notation transportation-detail">
-            <h3>Notes: </h3>
-            <div className="notation-text">{transportation.notation}</div>
-          </div>
-        )}
-        {(transportation.web ||
-          transportation.phone ||
-          transportation.email) && (
-          <div className="transportation-contact">
-            {transportation.web && (
+          )}
+          {transportation.priceWithCurrency && (
+            <div className="transportation-price transportation-detail">
+              <h3>Price: </h3>
               <div>
-                {transportation !== "" ? (
-                  !transportation.web.startsWith("https://") &&
-                  !transportation.web.startsWith("http://") &&
-                  transportation.web !== null &&
-                  transportation.web !== "" ? (
-                    <a href={"https://" + transportation.web} target="_blank">
-                      <FontAwesomeIcon icon={faGlobe} className="icon" />
-                      <div>Web:</div>
-                      <div className="link">{transportation.web}</div>
-                    </a>
-                  ) : (
-                    <a href={transportation.web} target="_blank">
-                      <FontAwesomeIcon icon={faGlobe} className="icon" />
-                      <div>Web:</div>
-                      <div className="link">{transportation.web}</div>
-                    </a>
-                  )
-                ) : (
-                  <p></p>
-                )}
+                {transportation.priceWithCurrency !== ""
+                  ? transportation.priceWithCurrency
+                  : ""}
               </div>
-            )}
-          </div>
-        )}
+            </div>
+          )}
+          {transportation.notation && (
+            <div className="transportation-notation transportation-detail">
+              <h3>Notes: </h3>
+              <div className="notation-text">{transportation.notation}</div>
+            </div>
+          )}
+        </div>
+
+        <img
+          className="details-illustration"
+          src={"../../../src/assets/illustrations/transportation.svg"}
+        ></img>
       </div>
+
+      {(transportation.web || transportation.phone || transportation.email) && (
+        <div className="transportation-contact">
+          {transportation.web && (
+            <div>
+              {transportation !== "" ? (
+                !transportation.web.startsWith("https://") &&
+                !transportation.web.startsWith("http://") &&
+                transportation.web !== null &&
+                transportation.web !== "" ? (
+                  <a href={"https://" + transportation.web} target="_blank">
+                    <FontAwesomeIcon icon={faGlobe} className="icon" />
+                    <div>Web:</div>
+                    <div className="link">{transportation.web}</div>
+                  </a>
+                ) : (
+                  <a href={transportation.web} target="_blank">
+                    <FontAwesomeIcon icon={faGlobe} className="icon" />
+                    <div>Web:</div>
+                    <div className="link">{transportation.web}</div>
+                  </a>
+                )
+              ) : (
+                <p></p>
+              )}
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
